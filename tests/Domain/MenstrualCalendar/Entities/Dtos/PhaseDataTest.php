b3cefc8c98c4baa855996d5c9db3d8b6a2859a84
wendellunny <wendelferreiraportugal@gmail.com>
2023-11-13 20:00:11 -0300

    Phase Data extensible class DTO unit test

---
+++ tests/Domain/MenstrualCalendar/Entities/Dtos/PhaseDataTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Domain\MenstrualCalendar\Entities\Dtos;
+
+use CicloMenstrual\Domain\MenstrualCalendar\Entities\Dtos\PhaseData;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+
+class PhaseDataTest extends TestCase
+{
+
+    private PhaseData $instance;
+    private DateTimeImmutable $initialDate;
+    private DateTimeImmutable $endDate;
+
+    /**
+     * Set up
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->setMocks();
+        $this->setInstance();
+    }
+
+    /**
+     * Set mocks
+     *
+     * @return void
+     */
+    private function setMocks(): void
+    {
+        $this->initialDate = new DateTimeImmutable('2023-11-25');
+        $this->endDate = new DateTimeImmutable('2023-12-17');
+    }
+
+    /**
+     * Set instance
+     *
+     * @return void
+     */
+    private function setInstance(): void
+    {
+        $this->instance = new PhaseData($this->initialDate, $this->endDate);
+    }
+    
+    /**
+     * Test getters
+     *
+     * @return void
+     */
+    public function testGetters(): void
+    {
+        $this->assertEquals(
+            $this->initialDate,
+            $this->instance->getInitialDate()
+        );
+
+        $this->assertEquals(
+            $this->endDate,
+            $this->instance->getEndDate()
+        );
+    }
+}
