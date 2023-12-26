d5e1b5f02dc6447d810efb0b20cc3725c31e4eee
wendellunny <wendelferreiraportugal@gmail.com>
2023-12-26 19:36:44 -0300

    refactor: Router test

--- tests/unit/Infrastructure/Services/Router/RouterTest.php
+++ tests/unit/Infrastructure/Services/Router/RouterTest.php
@@ -3,34 +3,51 @@
 namespace Tests\Infrastructure\Services\Router;
 
 use Aura\Router\RouterContainer;
+use CicloMenstrual\Infrastructure\Services\Router\Route;
 use CicloMenstrual\Infrastructure\Services\Router\Router;
 use DI\Container as DiContainer;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\RequestInterface;
 
+/**
+ * Test to use CicloMenstrual\Infrastructure\Services\Router\Router class
+ */
 class RouterTest extends TestCase
 {
-    private Router $instance;
+    private Router                      $instance;
     private MockObject|RequestInterface $requestMock;
-    private MockObject|RouterContainer $routerContainerMock;
-    private MockObject|DiContainer $diMock;
+    private MockObject|RouterContainer  $routerContainerMock;
+    private MockObject|DiContainer      $diMock;
 
+    /**
+     * Set up method
+     *
+     * @return void
+     */
     public function setUp(): void
     {
         $this->setMocks();
         $this->setInstance();
     }
 
+    /**
+     * Set mocks
+     *
+     * @return void
+     */
     private function setMocks(): void
     {
-        $this->requestMock = $this->createMock(RequestInterface::class);
-        $this->routerContainerMock = $this->createMock(RouterContainer::class);
-        $this->diMock = $this->createMock(DiContainer::class);
-
-        /** TODO: Continuar teste */
+        $this->requestMock          = $this->createMock(RequestInterface::class);
+        $this->routerContainerMock  = $this->createMock(RouterContainer::class);
+        $this->diMock               = $this->createMock(DiContainer::class);
     }
 
+    /**
+     * Set instance
+     *
+     * @return void
+     */
     private function setInstance(): void
     {
         $this->instance = new Router(
@@ -38,5 +55,25 @@ class RouterTest extends TestCase
             $this->routerContainerMock,
             $this->diMock
         );
+    }
+
+    /**
+     * Test configure and handle
+     *
+     * @return void
+     */
+    public function testConfigureAndHandle(): void
+    {
+        $this->diMock
+            ->expects($this->once())
+            ->method('get')
+            ->with(Route::class);
+
+        $this->assertEquals(
+            $this->instance,
+            $this->instance->configure('base/path/test')
+        );
+
+        $this->instance->handle();
     }
 }
